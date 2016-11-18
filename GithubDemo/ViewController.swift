b65918e6ef21b980ad0b002d72fc13df027ffc7c
ryancarlson <ryan.t.carlson@cru.org>
2016-11-18 14:16:06 -0500

    Create Swift Project with One Label

---
+++ GithubDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Ryan Carlson on 11/18/16.
+//  Copyright © 2016 Cru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        self.view.backgroundColor = .blue
+        self.helloWorldLabel.textColor = .yellow
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
